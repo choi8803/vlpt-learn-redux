932949137bca34b4c61bf78f9daef23e250df538
choi8803 <choijh225@naver.com>
2024-02-16 16:55:40 +0900

    second commit

---
+++ src/exercise.js
@@ -0,0 +1,88 @@
+import { createStore } from 'redux';
+
+const initialState = {
+  counter: 0,
+  text: '',
+  list: []
+};
+
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+const CHANGE_TEXT = 'CHANGE_TEXT';
+const ADD_TO_LIST = 'ADD_TO_LIST';
+
+function increase() {
+  return {
+    type: INCREASE,
+  }
+}
+
+const decrease = () => ({
+  type: DECREASE
+});
+
+const changeText = text => ({
+  type: CHANGE_TEXT,
+  text
+});
+
+const addToList = item => ({
+  type: ADD_TO_LIST,
+  item
+});
+
+function reducer(state=initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case CHANGE_TEXT:
+      return {
+        ...state,
+        text: action.text
+      };
+    case ADD_TO_LIST:
+      return {
+        ...state,
+        list: state.list.concat(action.item)
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer, initialState);
+
+console.log(store.getState());
+
+const listener = () => {
+  const state = store.getState();
+  console.log(state);
+};
+
+// 구독 (subscribe)
+// 스토어의 내장함수
+// subscribe 함수에 특정 함수를 전달해주면, 액션이 디스패치 되었을 때 마다 전달해준 함수가 호출
+const unsubscribe = store.subscribe(listener);
+// 구독을 해제하고 싶을 때는 unsubscribe() 를 호출하면 됩니다.
+
+
+// 액션들을 디스패치 해봅시다.
+store.dispatch(increase());
+store.dispatch(decrease());
+store.dispatch(changeText('안녕하세요'));
+store.dispatch(addToList({ id: 1, text: '와우' }));
+
+unsubscribe();
+store.dispatch(increase());
+store.dispatch(decrease());
+store.dispatch(changeText('안녕하세요'));
+store.dispatch(addToList({ id: 1, text: '와우' }));
+
