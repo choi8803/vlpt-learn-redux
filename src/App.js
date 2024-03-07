8dc6ca7eb930e5c8562eee023d33d9f8305fbfd9
choi8803 <choijh225@naver.com>
2024-03-08 00:53:36 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import CounterContainer from './components/CounterContainer';
+import TodosContainer from './components/TodosContainer';
 
 function App() {
   return (
     <div>
-      
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './exercise';
+import rootReducer from './modules';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  rootReducer, 
+  // 리덕스 모듈 설치없이 디버깅 가능
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store = {store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
